9aba22b4c11240bd9b66195b371e506967fde201
Kristina <kseparovi@student.unizd.hr>
2023-10-04 22:21:02 +0200

    Commit

--- src/ForLoop/MinMax.java
+++ src/ForLoop/MinMax.java
@@ -6,14 +6,16 @@ public class MinMax {
     public static void main(String[] args) {
 
         Scanner input = new Scanner(System.in);
-        int min = Integer.MAX_VALUE;
-        int max = Integer.MIN_VALUE;
         System.out.println("Molimo upisite 5 brojeva: ");
-
+        int unos = input.nextInt();
         int pokusaj = 5;
-        for (int i = 1; i <= pokusaj; i++) {
-            int n = input.nextInt();
+        int min = unos;
+        int max = unos;
 
+
+
+        for (int i = 1; i < pokusaj; i++) {
+            int n = input.nextInt();
             if (n == 0) {
                 System.out.println("Pogrešna vrijednost");
                 i--; // Smanjivanje broja pokušaja ako je unesen pogrešan broj
